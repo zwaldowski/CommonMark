ed188e2825a99155be5dcbf0143cce48b0c40f68
Zachary Waldowski <zach@waldowski.me>
2020-12-31 18:45:49 -0500

    Move rendering into its own file

--- Sources/CommonMark/Nodes/Node.swift
+++ Sources/CommonMark/Nodes/Node.swift
@@ -107,107 +107,6 @@ public class Node: Codable {
         return Node.create(for: cmark_node_parent(cmark_node))
     }
 
-    // MARK: - Rendering
-
-    /// Formats for rendering a document.
-    public enum RenderingFormat {
-        /// CommonMark
-        case commonmark
-
-        /// HTML
-        case html
-
-        /// XML
-        case xml
-
-        /// LaTeX
-        case latex
-
-        /// Manpage
-        case manpage
-    }
-
-    /// Options for rendering a CommonMark document.
-    public struct RenderingOptions: OptionSet {
-        public var rawValue: Int32
-
-        public init(rawValue: Int32 = CMARK_OPT_DEFAULT) {
-            self.rawValue = rawValue
-        }
-
-        /**
-         Render raw HTML and "unsafe" links.
-
-         A link is considered to be "unsafe"
-         if its scheme is `javascript:`, `vbscript:`, or `file:`,
-         or if its scheme is `data:`
-         and the MIME type of the encoded data isn't one of the following:
-
-         - `image/png`
-         - `image/gif`
-         - `image/jpeg`
-         - `image/webp`
-
-         By default,
-         raw HTML is replaced by a placeholder HTML comment.
-         Unsafe links are replaced by empty strings.
-
-         - Important: This option has an effect only when rendering HTML.
-         */
-        public static let unsafe = Self(rawValue: CMARK_OPT_UNSAFE)
-
-        /**
-         Render softbreak elements as spaces.
-
-         - Important: This option has no effect when rendering XML.
-         */
-        public static let noBreaks = Self(rawValue: CMARK_OPT_NOBREAKS)
-
-        /**
-         Render softbreak elements as hard line breaks.
-
-         - Important: This option has no effect when rendering XML.
-         */
-        public static let hardBreaks = Self(rawValue: CMARK_OPT_HARDBREAKS)
-
-        /**
-         Include a `data-sourcepos` attribute on all block elements
-         to map the rendered output to the source input.
-
-         - Important: This option has an effect only when rendering HTML or XML.
-         */
-        public static let includeSourcePosition = Self(rawValue: CMARK_OPT_SOURCEPOS)
-    }
-
-    /**
-     Render a document into a given format with the specified options.
-
-     - Parameters:
-        - format: The rendering format
-        - options: The rendering options
-        - width: The column width used to wrap lines for rendered output
-                 (`.commonmark`, `.man`, and `.latex` formats only).
-                 Must be a positive number.
-                 Pass `0` to prevent line wrapping.
-     - Returns: The rendered text.
-     */
-    public func render(format: RenderingFormat, options: RenderingOptions = [], width: Int = 0) -> String {
-        precondition(width >= 0)
-
-        switch format {
-        case .commonmark:
-            return String(cString: cmark_render_commonmark(cmark_node, options.rawValue, Int32(clamping: width)))
-        case .html:
-            return String(cString: cmark_render_html(cmark_node, options.rawValue, nil))
-        case .xml:
-            return String(cString: cmark_render_xml(cmark_node, options.rawValue))
-        case .latex:
-            return String(cString: cmark_render_latex(cmark_node, options.rawValue, Int32(clamping: width)))
-        case .manpage:
-            return String(cString: cmark_render_man(cmark_node, options.rawValue, Int32(clamping: width)))
-        }
-    }
-
     // MARK: - Codable
 
     public required convenience init(from decoder: Decoder) throws {
@@ -287,13 +186,5 @@ extension Node: Equatable {
 extension Node: Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(cmark_node)
-    }
-}
-
-// MARK: - CustomStringConvertible
-
-extension Node: CustomStringConvertible {
-    public var description: String {
-        return String(cString: cmark_render_commonmark(cmark_node, 0, 0))
     }
 }
